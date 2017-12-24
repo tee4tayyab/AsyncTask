e62f85f05992dfebf9c8a290f6d54a0611540624
Tayyab <tayyabrasheed330@gmail.com>
2017-12-24 18:37:42 +0500

    Initial commit

---
+++ app/src/main/java/com/example/tee/asynctask/Event.java
@@ -0,0 +1,18 @@
+package com.example.tee.asynctask;
+
+/**
+ * Created by Tee on 12/22/2017.
+ */
+
+public class Event {
+
+    private int percentage;
+
+    public Event(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+}
